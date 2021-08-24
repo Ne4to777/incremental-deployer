dbf14378feb3913dec7f599e67ae971710ec07b2
nybble <nybble@yandex-team.ru>
2021-08-24 16:03:49 +0300

    MARKETFRONTECH-3067 init config fix

--- src/run.ts
+++ src/run.ts
@@ -1,22 +1,25 @@
-import {readJSON, pipe, getProp, ifElse, pathJoin2, T, existsSync, writeJSONSync} from './utils';
+import {readJSON, pipe, getProp, ifElse, pathJoin2, T, existsSync, writeJSONSync, K} from './utils';
 import deploy from './deploy';
 import clear from './clear';
 import init from './init';
 import type {Config} from './types';
 
 type GetConfig = (args: any) => Config
+
+const projectDir = __dirname
+
 const getConfig: GetConfig = pipe([
     getProp('config', 'deployer.config.json'),
     pathJoin2(process.cwd()),
     ifElse(existsSync)([
         readJSON,
-        pipe([
-            writeJSONSync,
-            write => pipe([
-                readJSON,
-                write
-            ])('init.config.json')
-        ])
+        filePath => pipe([
+            pathJoin2(projectDir),
+            readJSON,
+            writeJSONSync(filePath),
+            K(filePath),
+            readJSON
+        ])('init.config.json')
     ])
 ])
 
